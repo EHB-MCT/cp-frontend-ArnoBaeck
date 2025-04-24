5d854cc2ea0bf00fb5eb13e11adf0ed9a079e3d3
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-24 17:17:32 +0200

    Updated SearchBar
    Changed for the future, when people search for a fairytale on the detail page, nothing will happen. With this change, you will automatically be redirected to the projects page when searching for a fairytale.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function SearchBar({ searchTerm, setSearchTerm }) {
-	return (
-		<input
-			type="text"
-			placeholder="Search for a Fairytale"
-			value={searchTerm}
-			onChange={(event) => setSearchTerm(event.target.value)}
-		/>
-	);
+	const navigate = useNavigate();
+
+	const handleChange = (event) => {
+		const value = event.target.value;
+		setSearchTerm(value);
+
+		if (location.pathname !== "/projects") {
+			navigate("/projects");
+		}
+	};
+
+	return <input type="text" placeholder="Search for a Fairytale" value={searchTerm} onChange={handleChange} />;
 }
 
 export default SearchBar;
