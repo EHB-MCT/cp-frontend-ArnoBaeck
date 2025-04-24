abe786d1c2aaecfc6082bde744fb7149b2cbc735
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-24 17:03:52 +0200

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+	return (
+		<footer>
+			<div className="logo">
+				<Link to="/">
+					<img src="./../public/logo.svg" alt="Logo" />
+				</Link>
+			</div>
+			<div className="about">
+				<h2>About us</h2>
+				<p>
+					Welcome to a world where fairy tales scroll with depth. Through parallax magic, we bring front-end stories to life
+					— one layer at a time. ✨
+				</p>
+			</div>
+            <div className="links">
+                <h2>Links</h2>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/making-of">Making-of</Link>
+                    </li>
+                </ul>
+            </div>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import Footer from "../components/footer";
 import { useState } from "react";
 
 function Layout() {
     const [searchTerm, setSearchTerm] = useState("");
-    
+
     return (
         <>
             <Navigation searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
 
             <Outlet context={{searchTerm}}/>
+            <Footer />
         </>
     )
 }
