0804234c8bd9c66f036d370afc08d9eb73fd4d9d
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-27 14:42:13 +0200

    Update App.jsx => added/removed todo's

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,10 @@ export default App
 
 //TODO:
 // 1. Render only the first 4 projects on the home page.
-// 2. Add a button to FairyTaleTile to navigate to the detail page.
-// 3. Render projects on the projects page.
+//DONE => // 2. Add a button to FairyTaleTile to navigate to the detail page.
+//DONE => // 3. Render projects on the projects page.
 // 4. Add a all projects button to the home page.
 // 5. Add a search button in the navigation bar.
-// 6. Styling.
+// 6. Add a delay on the search bar.
+// 7. Add a loading screen when data is being fetched.
+// 8. Styling.
