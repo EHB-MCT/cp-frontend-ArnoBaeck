e039499dde0853d5415a2a53d62b87338eec0cce
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-27 15:03:30 +0200

    Update Home.jsx => Added all projects button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import DATA from "../api/dummyData.json"
 import FairyTaleTile from "../components/FairyTaleTile";
 import { useOutletContext } from "react-router-dom";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     // The useOutletContext hook is used to access the context data passed from the parent route (Layout) to the child route (Home).
@@ -10,17 +12,22 @@ function Home() {
         FairyTale.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    const navigate = useNavigate();
+
     return (
-        <div className="FairyTales">
-            {filteredFairyTales.map((FairyTale) => (
-                <FairyTaleTile
-                    key={FairyTale.id}
-                    name={FairyTale.name}
-                    author={FairyTale.author}
-                    image={FairyTale.image}
-                />
-            ))}
-        </div>
+        <>
+            <div className="FairyTales">
+                {filteredFairyTales.map((FairyTale) => (
+                    <FairyTaleTile
+                        key={FairyTale.id}
+                        name={FairyTale.name}
+                        author={FairyTale.author}
+                        image={FairyTale.image}
+                    />
+                ))}
+            </div>
+            <Button label="All projects" onClick={() => navigate("/projects")} />
+        </>
     )
 }
 
