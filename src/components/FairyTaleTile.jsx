5ee56de6a775e8fa73f7148e3adcccdee24c9208
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-27 16:06:18 +0200

    Update FairyTaleTile.jsx => Button to go to detail page

--- src/components/FairyTaleTile.jsx
+++ src/components/FairyTaleTile.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router";
+
 function FairyTaleTile(props) {
+    const navigate = useNavigate();
+
     return (
         <div className="FairyTale">
             <div className="FairyTalePreview">
@@ -9,10 +13,10 @@ function FairyTaleTile(props) {
                         <p>{props.author}</p>
                     </div>
                     <div className="FairyTaleDetailButton">
-                        <button className="FairyTaleDetailButton" onClick={props.onClick}>
+                        <button onClick={() => navigate(`/making-of/${props.id}`)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                            <g clip-path="url(#clip0_62_130)">
-                                <path d="M8 10.6666V7.99992M8 5.33325H8.00667M14.6667 7.99992C14.6667 11.6818 11.6819 14.6666 8 14.6666C4.3181 14.6666 1.33333 11.6818 1.33333 7.99992C1.33333 4.31802 4.3181 1.33325 8 1.33325C11.6819 1.33325 14.6667 4.31802 14.6667 7.99992Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                            <g clipPath="url(#clip0_62_130)">
+                                <path d="M8 10.6666V7.99992M8 5.33325H8.00667M14.6667 7.99992C14.6667 11.6818 11.6819 14.6666 8 14.666 14.6666 1.33333 11.6818 1.33333 7.99992C1.33333 4.31802 4.3181 1.33325 8 1.33325C11.6819 1.33325 14.6667 4.31802 14.6667 7.99992Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
                             </g>
                             <defs>
                                 <clipPath id="clip0_62_130">
