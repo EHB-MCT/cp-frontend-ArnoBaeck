092eb09f4897d68959bdfa131651286463269700
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-27 16:09:47 +0200

    Update MakingOf.jsx => Now contains the actual component code

--- src/pages/MakingOf.jsx
+++ src/pages/MakingOf.jsx
@@ -1,7 +1,37 @@
+import { useParams } from "react-router-dom";
+import DATA from "../api/dummyData.json";
+import Button from "../components/Button";
+
 function DetailPage() {
-    return(
-        <p>Placeholder MAING-OF</p>
-    )
+	// Retrieves the 'id' parameter from the URL using the useParams hook.
+	const { id } = useParams();
+	const fairyTale = DATA.find((item) => item.id === parseInt(id));
+
+	if (!fairyTale) {
+		return <p>Fairytale not found!</p>;
+	}
+
+	return (
+		<div className="MakingOf">
+            <div className="FairyTaleDetails">
+                <h1>Making-of — {fairyTale.name}</h1>
+                <h2>By {fairyTale.author}</h2>
+            </div>
+			<img src={fairyTale.banner} alt={`${fairyTale.name} banner`} />
+			<div className="DescriptionDetails">
+                <div className="DetailContainer1">
+                    <h2>Explainer video</h2>
+                    <img src={fairyTale.video} alt={`${fairyTale.name} explainer video`} />
+                </div>
+                <div className="DetailContainer2">
+                    <h2>Description</h2>
+                    <p>{fairyTale.description}</p>
+                    <Button label="Visit website" onClick={() => console.log("Goto fairytale")} />
+                    {/* How will i redirect to the fairytale page? What is the most efficient way to do this? */}
+                </div>
+            </div>
+		</div>
+	);
 }
 
 export default DetailPage;
