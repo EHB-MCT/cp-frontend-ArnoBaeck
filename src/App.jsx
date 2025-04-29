b049530ab10e48ef10171cff8161a833b1d80cae
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-29 12:36:20 +0200

    Update App.jsx
    - Removed route
    - Changed todo's

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="projects" element={<Projects />} />
-                    <Route path="making-of" element={<DetailPage />} />
                     <Route path ="making-of/:id" element={<DetailPage />} />
 
                     {/* // Shows the 404 error page when the URL does not match any existing routes. */}
@@ -27,11 +26,11 @@ function App() {
 export default App
 
 //TODO:
-// 1. Render only the first 4 projects on the home page.
+//DONE => // 1. Render only the first 4 projects on the home page.
 //DONE => // 2. Add a button to FairyTaleTile to navigate to the detail page.
 //DONE => // 3. Render projects on the projects page.
 //DONE => // 4. Add a all projects button to the home page.
-// 5. Add a search button in the navigation bar.
+//Not doing that anymore, because you automatically search when filtering your data // 5. Add a search button in the navigation bar.
 // 6. Add a delay on the search bar.
 // 7. Add a loading screen when data is being fetched.
 // 8. Styling.
