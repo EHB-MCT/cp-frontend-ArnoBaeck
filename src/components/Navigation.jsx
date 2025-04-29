1696450a8346aed96acc73ed3e72a0891215c67c
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-29 12:39:59 +0200

    Update Navigation.jsx
    Updated navigation, now you see the red underline when a page is active

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,33 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 function Navigation({ searchTerm, setSearchTerm }) {
-    return (
-        <nav>
-            <div className="logoContainer">
-                <li>
-                    <Link to="/">
-                        <img src="./../public/logo.svg" alt="Logo" />
-                    </Link>
-                </li>
-            </div>
-            <div className="navigationContainer">
-                <div className="navigation">
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/projects">Projects</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="searchBar">
-                    <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                </div>
-            </div>
-        </nav>
-    );
+	const location = useLocation();
+
+	return (
+		<nav>
+			<div className="navigationFlexContainer">
+				<div className="logoContainer">
+					<li>
+						<Link to="/">
+							<img src="./../public/logo.svg" alt="Logo" />
+						</Link>
+					</li>
+				</div>
+				<div className="navigationContainer">
+					<div className="navigation">
+						<ul>
+							<li>
+								<Link to="/" className={location.pathname === "/" ? "active" : ""}>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link to="/projects" className={location.pathname === "/projects" ? "active" : ""}>
+									Projects
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="searchBar">
+						<SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+					</div>
+				</div>
+			</div>
+			<div className="divideLine"></div>
+		</nav>
+	);
 }
 
 export default Navigation;
