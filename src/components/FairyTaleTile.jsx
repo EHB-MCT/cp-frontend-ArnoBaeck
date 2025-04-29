b1345bf1846a9d8df0426804484da379f20e63de
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-29 12:38:58 +0200

    Update FairyTaleTile.jsx
    improved appearance

--- src/components/FairyTaleTile.jsx
+++ src/components/FairyTaleTile.jsx
@@ -9,20 +9,15 @@ function FairyTaleTile(props) {
                 <img src={props.image} alt={`Preview ${props.name}`} />
                 <div className="InfoContainer">
                     <div className="FairyTaleTitle">
-                        <h2>{props.name}</h2>
+                        <h2>{props.name.toUpperCase()}</h2>
                         <p>{props.author}</p>
                     </div>
                     <div className="FairyTaleDetailButton">
                         <button onClick={() => navigate(`/making-of/${props.id}`)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                            <g clipPath="url(#clip0_62_130)">
-                                <path d="M8 10.6666V7.99992M8 5.33325H8.00667M14.6667 7.99992C14.6667 11.6818 11.6819 14.6666 8 14.666 14.6666 1.33333 11.6818 1.33333 7.99992C1.33333 4.31802 4.3181 1.33325 8 1.33325C11.6819 1.33325 14.6667 4.31802 14.6667 7.99992Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16" fill="none">
+                            <g clipPath="url(#clip0_62_103)">
+                                <path d="M8.00001 10.6666V7.99992M8.00001 5.33325H8.00668M14.6667 7.99992C14.6667 11.6818 11.6819 14.6666 8.00001 14.6666C4.31811 14.6666 1.33334 11.6818 1.33334 7.99992C1.33334 4.31802 4.31811 1.33325 8.00001 1.33325C11.6819 1.33325 14.6667 4.31802 14.6667 7.99992Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
                             </g>
-                            <defs>
-                                <clipPath id="clip0_62_130">
-                                <rect width="16" height="16" fill="white"/>
-                                </clipPath>
-                            </defs>
                             </svg>
                         </button>
                     </div>
