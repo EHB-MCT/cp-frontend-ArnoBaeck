cdd54e09a1e21fa90bf7552ecb264f16f23a66e9
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-05-18 18:58:00 +0200

    Update SearchBar.jsx
    You now have a delay on the search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,45 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
-function SearchBar({ searchTerm, setSearchTerm }) {
+function SearchBar({ setSearchTerm }) {
 	const navigate = useNavigate();
 
-	// This function handles the change event of the search input field. When the users types in the search bar, they will be reditected to the projects page.
+	// State to hold the value of the search input
+	const [typedValue, setTypedValue] = useState("");
+	const timerRef = useRef(null);
+
+	// Effect to set a timeout for the search term update, but only if the input value changes
+	// This prevents the search term from being updated immediately, allowing for a delay
+	useEffect(() => {
+		clearTimeout(timerRef.current);
+
+		timerRef.current = setTimeout(() => {
+			setSearchTerm(typedValue);
+		}, 500);
+
+		// Cleanup function to clear the timeout if the typedValue changes again before the timeout completes
+		return () => clearTimeout(timerRef.current);
+	}, [typedValue]);
+
 	const handleChange = (event) => {
 		const value = event.target.value;
-		setSearchTerm(value);
+		setTypedValue(value);
 
+		// if the location is not already on the projects page, navigate to it
 		if (location.pathname !== "/projects") {
 			navigate("/projects");
 		}
 	};
 
-	return <input className="searchBar" type="text" placeholder="Search for a Fairytale" value={searchTerm} onChange={handleChange} />;
+	return (
+		<input
+			className="searchBar"
+			type="text"
+			placeholder="Search for a Fairytale"
+			value={typedValue}
+			onChange={handleChange}
+		/>
+	);
 }
 
 export default SearchBar;
