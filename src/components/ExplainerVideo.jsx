9d764b9c979d65ae0da852d93457c98e2373b16f
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-05-18 18:06:42 +0200

    Create ExplainerVideo.jsx

---
+++ src/components/ExplainerVideo.jsx
@@ -0,0 +1,10 @@
+function ExplainerVideo(props) {
+	return (
+		<div className="FairyTaleDetails">
+			<h1>Making-of — {props.name}</h1>
+			<h2>By {props.author}</h2>
+		</div>
+	);
+}
+
+export default ExplainerVideo;
