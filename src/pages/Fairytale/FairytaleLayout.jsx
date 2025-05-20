c6ca8d0376ea882cfb65daface8e2e334534122b
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-05-20 12:33:55 +0200

    Update FairytaleLayout

--- src/pages/Fairytale/FairytaleLayout.jsx
+++ src/pages/Fairytale/FairytaleLayout.jsx
@@ -1,10 +1,12 @@
 import FairytaleNavigation from "../../components/Fairytale/FairytaleNavigation"
+import { Outlet } from "react-router"
 
 function FairytaleLayout() {
 
     return (
         <div className="wrapper">
             <FairytaleNavigation />
+            <Outlet />
         </div>
     )
 }
