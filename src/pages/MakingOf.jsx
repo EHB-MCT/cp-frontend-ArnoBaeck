03c7e0a5b71737a7cfae79355e1fed7a36d06a30
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-05-24 17:54:46 +0200

    Update making of
    When someone has not submitted a link there is no button but a text saying that they did not submit a link.

--- src/pages/MakingOf.jsx
+++ src/pages/MakingOf.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useFetchFairytales from "../hooks/useFetchFairyTales";
 import Button from "../components/Button";
 import ExplainerVideo from "../components/ExplainerVideo";
+import { p } from "framer-motion/client";
 
 function MakingOf() {
 	// Retrieves the 'id' parameter from the URL using the useParams hook.
@@ -54,16 +55,14 @@ function MakingOf() {
 
 						<div className="visitWebsiteButton">
 							{/* Button to visit the fairytale site */}
-							<Button
-								label="Visit website"
-								onClick={() => {
-									if (fairyTale.fairytaleLink) {
-										window.open(fairyTale.fairytaleLink, "_blank");
-									} else {
-										console.log("No fairytale link available");
-									}
-								}}
-							/>
+							{fairyTale.fairytaleLink ? (
+								<Button
+									label="Visit website"
+									onClick={() => window.open(fairyTale.fairytaleLink, "_blank")}
+								/>
+							) : (
+								<p>This student has <strong>not</strong> submitted a link to his/her fairy tale</p>
+							)}
 						</div>
 					</div>
 				</div>
